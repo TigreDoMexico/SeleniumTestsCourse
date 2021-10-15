7aca617da1c5b545d12622dbe3078829064d03c9
David Tigre <tigredavid007@gmail.com>
2021-10-15 16:31:56 -0300

    Mobile Tests

---
+++ Alura.LeilaoOnline.Selenium/Tests/AoNavegarParaHomeMobile.cs
@@ -0,0 +1,53 @@
+﻿using Alura.LeilaoOnline.Selenium.PageObjects;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Chromium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium.Tests
+{
+    public class AoNavegarParaHomeMobile : IDisposable
+    {
+        private ChromeDriver driver;
+        private HomeNaoLogadaPO register;
+
+        public AoNavegarParaHomeMobile()
+        {
+            var options = new ChromeOptions();
+            var device = new ChromiumMobileEmulationDeviceSettings();
+            device.Width = 400;
+            device.Height = 800;
+            device.UserAgent = "Customizada";
+
+            options.EnableMobileEmulation(device);
+
+            driver = new ChromeDriver(".", options);
+            register = new HomeNaoLogadaPO(driver);
+        }
+
+        [Fact]
+        public void DadaLargura400DeveMostrarMenuMobile()
+        {
+            // Arrange
+
+            // Act
+            register.AcessarHome();
+
+            // Assert
+            Assert.True(register.Menu.MenuMobileVisible);
+        }
+
+        [Fact]
+        public void DadaLargura993NaoDeveMostrarMenuMobile()
+        {
+            // TODO
+        }
+
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+    }
+}
